eee404caaeaf3fdfd459f0fd34c7ee1e9ff138c3
Tamjid047 <53190904+Tamjid047@users.noreply.github.com>
2019-08-06 22:03:27 +0600

    initial

---
+++ 2_adding number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+    /*adding two predefined numbers*/
+
+    int a=100,b=200,c;
+    printf("your numbers are : %d and %d\n",a,b);
+    c=a+b;
+    printf("summation :%d",c);
+
+
+}
---
+++ ReadMe.txt
@@ -0,0 +1,3 @@
+Task:
+------
+Write a C code to add 100 and 200.
